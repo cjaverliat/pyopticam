a2c48ad2e38af1f91ad1522da9e675deff4ae25b
Eliott Zimmermann <37654999+eliottzim@users.noreply.github.com>
2026-04-28 11:54:56 +0200

    Update activate.bat

--- activate.bat
+++ activate.bat
@@ -1,8 +1,7 @@
 @echo off
 if not defined OTCSDK_ROOT_DIR (
-    echo Error: OTCSDK_ROOT_DIR is not set. Please set it to the OptiTrack Camera SDK root directory.
-    echo   set OTCSDK_ROOT_DIR=C:\path\to\CameraSDK
+    echo "Error: OTCSDK_ROOT_DIR is not set. Please set it to the OptiTrack Camera SDK root directory. Set OTCSDK_ROOT_DIR=C:\path\to\CameraSDK"
 ) else (
-    set CMAKE_ARGS=-DOTCSDK_ROOT_DIR="%OTCSDK_ROOT_DIR%"
-    set PATH=%OTCSDK_ROOT_DIR%\bin;%PATH%
+    set "CMAKE_ARGS=-DOTCSDK_ROOT_DIR=%OTCSDK_ROOT_DIR%"
+    set "PATH=%OTCSDK_ROOT_DIR%\bin;%PATH%"
 )
